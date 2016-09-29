9ed8ba725a5dc680faa1fbac3697a9d67382b3e9
nishkixs <nishon.radhakrishnan@gmail.com>
2016-09-29 15:40:03 -0400

    created curl request

--- scripts/sign-up.sh
+++ scripts/sign-up.sh
@@ -4,9 +4,9 @@ curl --include --request POST http://localhost:3000/sign-up \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "an@example.email",
-      "password": "an example password",
-      "password_confirmation": "an example password"
+      "email": "big111@hotmail.com",
+      "password": "1",
+      "password_confirmation": "1"
     }
   }'
 
